8243fe1c3928172e94c6acab95185be754d00c57
MarkMGill <40337791+MarkMGill@users.noreply.github.com>
2020-02-26 10:10:49 -0500

    Updates

---
+++ precache-manifest.1929add9d99675370844fcb5e0f308e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ec5707f421dab070b2bf582554ca7e",
+    "url": "/Newscast/index.html"
+  },
+  {
+    "revision": "b3d55342d5b5e86012e6",
+    "url": "/Newscast/static/js/2.48bb6f38.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Newscast/static/js/2.48bb6f38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc4b38e67d2484463a74",
+    "url": "/Newscast/static/js/main.fd8fbea5.chunk.js"
+  },
+  {
+    "revision": "8de729e4210aff2710b1",
+    "url": "/Newscast/static/js/runtime-main.0835e248.js"
+  }
+]);
